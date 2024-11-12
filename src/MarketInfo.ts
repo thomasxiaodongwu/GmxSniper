53e5aba43791102d1257c287230d18cd7c6618e3
thomasxiaodongwu <755147511@qq.com>
2024-11-12 16:41:23 +0800

    market detail information

--- src/MarketInfo.ts
+++ src/MarketInfo.ts
@@ -1,6 +1,7 @@
 import Web3 from 'web3';
 import path from "path";
 import fs from "fs";
+import { BigNumber, BigNumberish, ethers } from "ethers";
 
 console.log(__dirname);
 const web3 = new Web3('https://arb-mainnet.g.alchemy.com/v2/Z08SBQ9CRg6OC8LhlObkEqWrDJyjY2CS');
@@ -22,23 +23,61 @@ async function callGetAddress(key: string) {
         console.log('callGetAddress:', address);
         return address;
     } catch (error) {
-        console.error('Error calling contract method:', error);
+        console.error('Error callGetAddress:', error);
     }
 }
 
 async function callGetMarkets() {
     try {
-        const market = await contractReader.methods.getMarkets("0xFD70de6b91282D8017aA4E741e9Ae325CAb992d8c", 0, 100).call();
+        const market = await contractReader.methods.getMarkets("0xFD70de6b91282D8017aA4E741e9Ae325CAb992d8", 0, 100).call();
         console.log('callGetMarkets:', JSON.stringify(market));
+        return market;
     } catch (error) {
-        console.error('Error calling contract method:', error);
+        console.error('Error callGetMarkets:', error);
     }
+}
+
+function getTickersUrl() {
+    return "https://arbitrum-api.gmxinfra2.io/prices/tickers";
+}
+
+// @ts-ignore
+function getTokenPrice({ token, pricesByTokenAddress }) {
+    let price = pricesByTokenAddress[token.toLowerCase()];
+    return price;
 }
 
 async function main() {
     try {
+        const marketKey = "0x47c031236e19d024b42f8AE6780E44A573170703";
+        const tokenPricesResponse = await fetch(getTickersUrl());
+        const tokenPrices = await tokenPricesResponse.json();
+        const pricesByTokenAddress = {};
 
-        await callGetMarkets();
+        for (const tokenPrice of tokenPrices) {
+            // @ts-ignore
+            pricesByTokenAddress[tokenPrice.tokenAddress.toLowerCase()] = {
+                min: ethers.BigNumber.from(tokenPrice.minPrice),
+                max: ethers.BigNumber.from(tokenPrice.maxPrice),
+            };
+        }
+        const markets = [...(await callGetMarkets())];
+        let marketPrices = {}
+        for (const market of markets) {
+            console.log(market.marketToken);
+            if(marketKey === market.marketToken) {
+                marketPrices = {
+                    indexTokenPrice: getTokenPrice({token: market.indexToken, pricesByTokenAddress}),
+                    longTokenPrice: getTokenPrice({token: market.longToken, pricesByTokenAddress}),
+                    shortTokenPrice: getTokenPrice({token: market.shortToken, pricesByTokenAddress}),
+                };
+                console.log(marketPrices);
+                break;
+            }
+        }
+        console.log("get into market detail:");
+        const marketInfos = await contractReader.methods.getMarketInfo("0xFD70de6b91282D8017aA4E741e9Ae325CAb992d8", marketPrices, marketKey).call();
+        console.log(JSON.stringify(marketInfos));
     } catch (error) {
         console.error('Error in main:', error);
     }
--- src/Transform.ts
+++ src/Transform.ts
@@ -1,11 +1,10 @@
-import Web3 from 'web3';
+import { BigNumber } from 'bignumber.js';
 
-// 初始化 Web3
-const web3 = new Web3();
+function calculate(a: string): BigNumber {
+    const divisor = new BigNumber(2).pow(96);
+    return new BigNumber(a).dividedBy(divisor);
+}
 
-// 假设 uint256Value 是一个十六进制的字符串
-const uint256Value = '41458205439508593024621185058880012909'; // 示例值
-
-// 转换为十进制字符串
-const decimalValue = web3.utils.toBN(uint256Value).toString();
-console.log('Decimal:', decimalValue);
+const uint256Value = '41458205439508593024621185058880012909';
+const result = calculate(uint256Value);
+console.log('Result:', result.toString());
